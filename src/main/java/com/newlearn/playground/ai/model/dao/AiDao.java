3b1355dd4d82fa6887d4d7dc41a4a57d5e2efdb7
maxthenobody <hjy6296@gmail.com>
2025-07-22 19:30:52 +0900

    20250722_1930

--- src/main/java/com/newlearn/playground/ai/model/dao/AiDao.java
+++ src/main/java/com/newlearn/playground/ai/model/dao/AiDao.java
@@ -3,12 +3,12 @@ package com.newlearn.playground.ai.model.dao;
 import java.util.List;
 import java.util.Map;
 
-import com.newlearn.playground.ai.model.vo.AI;
+import com.newlearn.playground.ai.model.vo.Ai;
 import com.newlearn.playground.ai.model.vo.AiChatSession;
 
 public interface AiDao {
 
-	List<AI> getAiList();
+	List<Ai> getAiList();
 
 	int checkUsedBefore(Map<String, Object> dbParamMap);
 
